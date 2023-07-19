26e76eeee04678ba5bfb9aec57a012fa711cb12b
Seong Kim <runmath@gmail.com>
2023-07-18 21:31:21 -0600

    test done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+
+    # if the number of items is 2, connect the two with and without a comma,
+    # and if the number is 3 or more, the last item will be commected with and anyway
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+    
+    # if the number is 3 or more, connect each item with a comma and a space,
+    # since the last item has been connected with and already
+    if len(items) > 2:
+        return ", ".join(items)
+    else:
+        # if the number is 0 or 1, connect the item with nothing
+        return " ".join(items)
